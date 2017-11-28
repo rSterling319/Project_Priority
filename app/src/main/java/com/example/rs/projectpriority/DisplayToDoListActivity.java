4cc4e89f3e06dfb726faeeb6586a1c36c61f4e05
Richard Sterling <rsterlin@mavs.coloradomesa.edu>
2017-11-27 22:58:12 -0700

    Created java class to display the to do list activity

---
+++ app/src/main/java/com/example/rs/projectpriority/DisplayToDoListActivity.java
@@ -0,0 +1,12 @@
+package com.example.rs.projectpriority;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rs on 11/27/17.
+ */
+
+public class DisplayToDoListActivity extends AppCompatActivity {
+
+}
